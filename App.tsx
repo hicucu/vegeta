7aa11f6ab89728748a48c42f88db36432f124cd4
Deulyi Kim <deul@DEUL.local>
2021-01-06 23:41:01 +0900

    feat: chach images, fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading  from 'expo-app-loading';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import {Text, Image} from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const cacheImages = (images:Array<string|number>)=>{
+  return images.map(image=>{
+    if(typeof image === "string"){
+      return Image.prefetch(image);
+    }
+    else return Asset.fromModule(image).downloadAsync();
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const cacheFonts = (font:any)=>{
+  return font.map((font:string) => Font.loadAsync(font));
+}
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = ()=>{
+    const images = cacheImages([require("./assets/splash.png")]);
+
+    const fonts = cacheFonts([Ionicons.font]);
+
+    return Promise.all([...images, ...fonts]);
+  };
+  const onFinish = ()=>setIsReady(true);
+  return isReady ? <Text>Ready</Text> : <
+    // @ts-ignore
+    AppLoading startAsync={loadAssets} 
+    onError={(error)=>console.error(error)}
+    onFinish={onFinish} />;
+}
